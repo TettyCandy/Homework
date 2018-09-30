92b5686c7fb44f46b70b14f4369f6879ff604404
TettyCandy <43089207+TettyCandy@users.noreply.github.com>
2018-09-30 21:59:03 +0300

    Add files via upload

---
+++ 09.23.2018/кватернионы.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct Quaternion
+{
+	double w;
+	double x;
+	double y;
+	double z;
+	
+	Quaternion (double w1, double x1, double y1, double z1): w(w1), x(x1), y(y1), z(z1)  //конструктор
+	{		
+	}
+	Quaternion summ( Quaternion& a, Quaternion& b)                                       //сумма
+	{
+		return Quaternion(a.w + b.w , a.x + b.x , a.y + b.y , a.z + b.z);
+	}
+	Quaternion diff( Quaternion& a, Quaternion& b)                                       //разность
+	{
+		return Quaternion(a.w - b.w , a.x - b.x , a.y - b.y , a.z - b.z);
+	}
+	Quaternion mul(Quaternion& a, Quaternion& b)                                         //умножение
+	{
+		Quaternion c(0,0,0,0);
+		c.w=a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z;   
+		c.x=a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y;
+		c.y=a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x;
+		c.z=a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w;
+		return c;
+	}
+	
+};
+int main()
+{
+	Quaternion a(1,2,1,3);
+	Quaternion b(0,4,2,1);
+	Quaternion c(0,0,0,0);
+	c=c.mul(a,b);
+	a=a.summ(a,b);
+	cout<<a.w<<"+"<<a.x<<"i+"<<a.y<<"j+"<<a.z<<"k"<<endl;
+	cout<<c.w<<"+"<<c.x<<"i+"<<c.y<<"j+"<<c.z<<"k"<<endl;
+} 
